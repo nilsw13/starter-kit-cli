fd2222260197492608682139507f2bb27d4007ea
nilsw13 <nils.wenting@laplateforme.io>
2025-04-08 14:49:09 +0200

    First commit

---
+++ src/main/java/com/nilsw13/starter_kit_cli/StarterKitCliApplication.java
@@ -0,0 +1,13 @@
+package com.nilsw13.starter_kit_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterKitCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterKitCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nilsw13/starter_kit_cli/StarterKitCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilsw13.starter_kit_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterKitCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
