b484fd73f8c138b0e3d0cd6b9ab14f6dc7b178b6
nilsw13 <nils.wenting@laplateforme.io>
2025-04-09 17:39:48 +0200

    .

--- src/main/java/com/nilsw13/starter_kit_cli/StarterKitCliApplication.java
+++ src/main/java/com/nilsw13/starter_kit_cli/StarterKitCliApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StarterKitCliApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(StarterKitCliApplication.class, args);
+
 	}
 
 }
