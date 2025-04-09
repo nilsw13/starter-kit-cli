5f4111bcc453e5fcc5579cae9659e933d27c712a
nilsw13 <nils.wenting@laplateforme.io>
2025-04-09 17:39:30 +0200

    Add GithubService to handle github methods

--- src/main/java/com/nilsw13/starter_kit_cli/service/GithubService.java
+++ src/main/java/com/nilsw13/starter_kit_cli/service/GithubService.java
@@ -1,14 +1,83 @@
 package com.nilsw13.starter_kit_cli.service;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Component
 public class GithubService {
-    private static final String GITHUB_REPO_URL = "https://github.com/nilsw13/spring-react";
+    private static final String GITHUB_REPO_URL = "https://github.com/nilsw13/spring-react.git";
 
-    public Git cloneDefaultRepo(String projectName) {
-        Git git = new Git()
+    public Git cloneDefaultRepo(String projectName) throws GitAPIException {
+
+
+
+        try{
+            Path currentDirectory = Paths.get("./");
+            Path projectDirectory = currentDirectory.resolve(projectName);
+            File projectFolder = projectDirectory.toFile();
+            if (!projectFolder.exists()) {
+                boolean created = projectFolder.mkdirs();
+                if (!created) {
+                    System.out.println("Impossible de créer le dossier : " + projectFolder.getAbsolutePath());
+                    return null;
+                }
+            }
+
+            Git git = Git.cloneRepository()
+                    .setURI(GITHUB_REPO_URL)
+                    .setDirectory(projectFolder)
+                    .call();
+
+             // maintenant il faut naviguer dans le nouveau repos pour supprimer le dossier git et ensuite en crée un nouveau
+            File gitDir = new File(projectFolder, ".git");
+            if (gitDir.exists()){
+                deleteDirectory(gitDir);
+            }
+
+            // maintenant il faut init un nouveau depot
+            git = Git.init().setDirectory(projectFolder).call();
+
+             return git;
+
+        } catch (GitAPIException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        return null;
+    }
+
+    public void deleteDirectory(File dir) throws IOException {
+        if(dir.exists()) {
+            File[] files = dir.listFiles();
+            if (files!=null){
+                for (File file : files) {
+                    if(file.isDirectory()){
+                        deleteDirectory(file);
+                    }else {
+                        boolean deleted = file.delete();
+                        if(!deleted) {
+                            throw new IOException("Error while deleting .git repository");
+                        }
+                    }
+                }
+
+            }
+            boolean deleted = dir.delete();
+            if(!deleted) {
+                throw  new IOException("Error deleting " + dir );
+            }
+
+
+        }
+
+
     }
 }
